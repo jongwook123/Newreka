fdc5f51136a0d6788c54496e9d6af7a43233d42c
DESKTOP-DOGVPUB\SSAFY <cbchan644@naver.com>
2023-09-04 10:27:37 +0900

    BackEnd init

---
+++ backend/src/main/java/com/d103/newreka/NewrekaApplication.java
@@ -0,0 +1,13 @@
+package com.d103.newreka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewrekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewrekaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/d103/newreka/NewrekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d103.newreka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewrekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
