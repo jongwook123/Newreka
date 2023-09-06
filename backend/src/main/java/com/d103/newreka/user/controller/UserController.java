8c030a13f63a740c2b3ad43b591d612bdf4e548e
천병찬 <cbchan644@naver.com>
2023-09-06 17:50:01 +0900

    Feat. ERD작성 , User Controller, domain 작성

--- backend/src/main/java/com/d103/newreka/user/controller/UserController.java
+++ backend/src/main/java/com/d103/newreka/user/controller/UserController.java
@@ -1,5 +1,13 @@
 package com.d103.newreka.user.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+
 }
