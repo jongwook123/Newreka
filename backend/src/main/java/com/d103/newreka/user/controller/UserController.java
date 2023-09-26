c408df842844df1a51f0f2a13ebcee7b1311118d
천병찬 <cbchan644@naver.com>
2023-09-26 13:20:56 +0900

    Feat: JWT로그인기능 구현

--- backend/src/main/java/com/d103/newreka/user/controller/UserController.java
+++ backend/src/main/java/com/d103/newreka/user/controller/UserController.java
@@ -1,13 +1,39 @@
 package com.d103.newreka.user.controller;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
+import com.d103.newreka.global.dto.GlobalResDto;
+import com.d103.newreka.user.dto.LoginReqDto;
+import com.d103.newreka.user.dto.UserReqDto;
+import com.d103.newreka.user.service.UserService;
 
 @RestController
 @RequestMapping("/user")
-@RequiredArgsConstructor
 public class UserController {
+	private final UserService userService;
 
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@PostMapping("/signup")
+	public ResponseEntity<GlobalResDto> signup(@Valid @RequestBody UserReqDto userReqDto) {
+		System.out.println(userReqDto);
+		GlobalResDto response = userService.signup(userReqDto);
+		return ResponseEntity.status(response.getStatusCode()).body(response);
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<GlobalResDto> login(@Valid @RequestBody LoginReqDto loginReqDto,
+		HttpServletResponse response) {
+		GlobalResDto globalResponse = userService.login(loginReqDto, response);
+		return ResponseEntity.status(globalResponse.getStatusCode()).body(globalResponse);
+	}
 }
