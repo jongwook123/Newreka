2b4e8784ad9f426f285ff5a5c9a4dcaf00c45b81
DESKTOP-DOGVPUB\SSAFY <corudgh123@naver.com>
2023-09-26 17:43:30 +0900

    Feat: load list

--- backend/src/main/java/com/d103/newreka/hottopic/controller/KeyWordController.java
+++ backend/src/main/java/com/d103/newreka/hottopic/controller/KeyWordController.java
@@ -4,16 +4,16 @@ import com.d103.newreka.hottopic.dto.KeyWordDto;
 import com.d103.newreka.hottopic.dto.TimeDto;
 import com.d103.newreka.hottopic.service.KeyWordService;
 import com.d103.newreka.hottopic.service.TimeService;
+import com.d103.newreka.quiz.dto.QuizDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/hottopic/keyword")
@@ -34,6 +34,26 @@ public class KeyWordController {
         }catch (Exception e){
             resultMap.put("messege", "fail: "+ e.getClass().getSimpleName());
             System.out.println(e);
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
+    }
+
+    @GetMapping("/keyWordList")
+    public ResponseEntity<Map<String, Object>> add() {
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = null;
+        try {
+            List<KeyWordDto> keyWordList = keyWordService.getKeyWordList()
+                    .stream()
+                    .map(m -> KeyWordDto.fromEntity(m))
+                    .collect(Collectors.toList());
+            resultMap.put("quizList", keyWordList);
+            resultMap.put("message", "success");
+            status = HttpStatus.ACCEPTED;
+        } catch (Exception e) {
+//            logger.error("질문 검색 실패", e);
+            resultMap.put("message", "fail: " + e.getClass().getSimpleName());
             status = HttpStatus.INTERNAL_SERVER_ERROR;
         }
         return new ResponseEntity<Map<String, Object>>(resultMap, status);
--- backend/src/main/java/com/d103/newreka/hottopic/dto/KeyWordDto.java
+++ backend/src/main/java/com/d103/newreka/hottopic/dto/KeyWordDto.java
@@ -1,8 +1,10 @@
 package com.d103.newreka.hottopic.dto;
 
 import com.d103.newreka.hottopic.domain.Article;
+import com.d103.newreka.hottopic.domain.KeyWord;
 import com.d103.newreka.hottopic.domain.Time;
 import com.d103.newreka.quiz.domain.Quiz;
+import com.d103.newreka.quiz.dto.QuizDto;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
@@ -11,7 +13,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
+@Builder
 @Getter
 @Setter
 @Data
@@ -22,5 +24,15 @@ public class KeyWordDto {
     String summary;
     String category;
     Long timeId;
+
+    public static KeyWordDto fromEntity(KeyWord keyWord){
+        return KeyWordDto.builder()
+                .keyWordId(keyWord.getKeyWordId())
+                .name(keyWord.getName())
+                .summary(keyWord.getSummary())
+                .category(keyWord.getCategory())
+                .timeId(keyWord.getTime().getTimeId())
+                .build();
+    }
 
 }
