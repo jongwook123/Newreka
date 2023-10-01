4a689f32d99abcadb78747c4b4e8c741d09eb4e0
박종욱 <pju7429@naver.com>
2023-10-01 15:25:54 +0900

    Fix: merge 오류해결

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store'; // 실제 Redux store 경로에 따라 수정
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-        <App />
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
