071cfd8805ffb295ecabad358ecf0ef17273888c
jeonghwanKim <doyun30@naver.com>
2023-10-04 20:57:44 +0900

    Feat. Scrap 조회

--- backend/src/main/java/com/d103/newreka/user/repo/ScrapRepo.java
+++ backend/src/main/java/com/d103/newreka/user/repo/ScrapRepo.java
@@ -1,12 +1,13 @@
 package com.d103.newreka.user.repo;
 
 import com.d103.newreka.user.domain.Scrap;
+import com.d103.newreka.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ScrapRepo extends JpaRepository<Scrap, Long> {
-    List<Scrap> findByUser_Id(Long userId);
+public interface ScrapRepo extends JpaRepository<Scrap, User> {
+    List<Scrap> findByUserId(User user);
 }
