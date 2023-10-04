df470970bf3ddc9e660e1a5325273fa8fad15cff
윤우혁 <squareyun@gmail.com>
2023-10-04 14:24:23 +0900

    Feat: 시간별 키워드 조회 API 추가

--- backend/src/main/java/com/d103/newreka/hottopic/repo/TimeRepo.java
+++ backend/src/main/java/com/d103/newreka/hottopic/repo/TimeRepo.java
@@ -4,7 +4,10 @@ import com.d103.newreka.hottopic.domain.Time;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface TimeRepo extends JpaRepository<Time, Long> {
 
+    Time findByTime(LocalDateTime time);
 }
--- backend/src/main/java/com/d103/newreka/hottopic/service/KeyWordService.java
+++ backend/src/main/java/com/d103/newreka/hottopic/service/KeyWordService.java
@@ -1,47 +1,68 @@
 package com.d103.newreka.hottopic.service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.d103.newreka.hottopic.domain.KeyWord;
 import com.d103.newreka.hottopic.domain.Time;
 import com.d103.newreka.hottopic.dto.KeyWordDto;
 import com.d103.newreka.hottopic.repo.KeyWordRepo;
 import com.d103.newreka.hottopic.repo.TimeRepo;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class KeyWordService {
 
-	private final KeyWordRepo keyWordRepo;
-	private final TimeRepo timeRepo;
+    private final KeyWordRepo keyWordRepo;
+    private final TimeRepo timeRepo;
 
-	@Transactional
-	public void saveKeyWord(KeyWordDto keyWordDto) {
-		Time time = timeRepo.getReferenceById(keyWordDto.getTimeId());
-		KeyWord keyWord = KeyWord.builder()
-			.keyWordId(keyWordDto.getKeyWordId())
-			.name(keyWordDto.getName())
-			.summary(keyWordDto.getSummary())
-			.category(keyWordDto.getCategory())
-			.time(time)
-			.build();
-		keyWordRepo.save(keyWord);
-	}
+    @Transactional
+    public void saveKeyWord(KeyWordDto keyWordDto) {
+        Time time = timeRepo.getReferenceById(keyWordDto.getTimeId());
+        KeyWord keyWord = KeyWord.builder()
+                .keyWordId(keyWordDto.getKeyWordId())
+                .name(keyWordDto.getName())
+                .summary(keyWordDto.getSummary())
+                .category(keyWordDto.getCategory())
+                .time(time)
+                .build();
+        keyWordRepo.save(keyWord);
+    }
 
-	@Transactional
-	public List<KeyWordDto> getKeyWordList() {
-		List<KeyWord> keyWords = keyWordRepo.findTop10ByOrderByKeyWordIdDesc();
-		List<KeyWordDto> keyWordsDtos = keyWords.stream()
-			.map(KeyWordDto::fromEntity)
-			.collect(Collectors.toList());
-		return keyWordsDtos;
-	}
+    public List<KeyWordDto> getKeyWordList() {
+        List<KeyWord> keyWords = keyWordRepo.findTop10ByOrderByKeyWordIdDesc();
+        List<KeyWordDto> keyWordsDtos = keyWords.stream()
+                .map(KeyWordDto::fromEntity)
+                .collect(Collectors.toList());
+        return keyWordsDtos;
+    }
 
+    /**
+     * 시간을 기준으로 키워드 검색후 반환
+     */
+    public List<KeyWordDto> getKeyWordListWithTime(String reqTime) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmm");
+        LocalDateTime dateTime = LocalDateTime.parse(reqTime, formatter);
+
+        Time timeEntity = timeRepo.findByTime(dateTime);
+
+        if (timeEntity == null) {
+            throw new RuntimeException("해당하는 시간의 데이터가 없습니다.");
+        }
+
+        return timeEntity.getKeyWords().stream()
+                .map(KeyWordDto::fromEntity)
+                .collect(Collectors.collectingAndThen(Collectors.toList(), list -> {
+                    Collections.reverse(list);
+                    return list;
+                }));
+    }
 }
