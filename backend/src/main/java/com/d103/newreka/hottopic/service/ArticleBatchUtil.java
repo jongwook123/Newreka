f91cb0c4204b7bc6a197517ff55555872b2f62c1
윤우혁 <squareyun@gmail.com>
2023-10-05 13:48:07 +0900

    Fix: 따옴표 문제 해결

--- backend/src/main/java/com/d103/newreka/hottopic/service/ArticleBatchUtil.java
+++ backend/src/main/java/com/d103/newreka/hottopic/service/ArticleBatchUtil.java
@@ -1,20 +1,16 @@
 package com.d103.newreka.hottopic.service;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+import com.d103.newreka.hottopic.domain.KeyWord;
+import com.d103.newreka.hottopic.dto.ArticleDto;
+import com.d103.newreka.hottopic.repo.KeyWordRepo;
 import com.d103.newreka.quiz.domain.Quiz;
 import com.d103.newreka.quiz.repo.QuizRepo;
 import com.knuddels.jtokkit.Encodings;
 import com.knuddels.jtokkit.api.Encoding;
 import com.knuddels.jtokkit.api.EncodingRegistry;
 import com.knuddels.jtokkit.api.ModelType;
+import com.nimbusds.jose.shaded.json.JSONObject;
+import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -28,83 +24,86 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.d103.newreka.hottopic.domain.KeyWord;
-import com.d103.newreka.hottopic.dto.ArticleDto;
-import com.d103.newreka.hottopic.repo.KeyWordRepo;
-import com.nimbusds.jose.shaded.json.JSONObject;
-
-import lombok.RequiredArgsConstructor;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class ArticleBatchUtil {
 
-	private final NewsService newsService;
-	private final ArticleService articleService;
-	private final KeyWordRepo keyWordRepo;
-	private final QuizRepo quizRepo;
+    private final NewsService newsService;
+    private final ArticleService articleService;
+    private final KeyWordRepo keyWordRepo;
+    private final QuizRepo quizRepo;
 
-	private static final Logger logger = LoggerFactory.getLogger(ArticleBatchUtil.class);
-	private static String client_id = "hevtdy8yus";
-	private static String client_secret = "11vHPak7llNgpKSm3wx5S7U8Vlsw1zUnZPyWptvh";
-	private static String url = "https://naveropenapi.apigw.ntruss.com/text-summary/v1/summarize";
-	private static String url2 = "https://api.openai.com/v1/chat/completions";
-	private static String gpt = "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";
-	private static CloseableHttpClient httpClient = HttpClients.createDefault();
+    private static final Logger logger = LoggerFactory.getLogger(ArticleBatchUtil.class);
+    private static String client_id = "hevtdy8yus";
+    private static String client_secret = "11vHPak7llNgpKSm3wx5S7U8Vlsw1zUnZPyWptvh";
+    private static String url = "https://naveropenapi.apigw.ntruss.com/text-summary/v1/summarize";
+    private static String url2 = "https://api.openai.com/v1/chat/completions";
+    private static String gpt = "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";
+    private static CloseableHttpClient httpClient = HttpClients.createDefault();
 
-	@Scheduled(cron = "0 1/10 * * * ?") // 1분, 11분, 21분, ... 단위로 스케줄러 등록
-	public void insertData() {
-		try {
-			List<KeyWord> keywordList = keyWordRepo.findTop10ByOrderByKeyWordIdDesc();
-			Collections.reverse(keywordList); // 순서가 반대로 불러와져서 있어서 뒤집기
+    @Scheduled(cron = "0 6/10 * * * ?") // 1분, 11분, 21분, ... 단위로 스케줄러 등록
+    public void insertData() {
+        try {
+            List<KeyWord> keywordList = keyWordRepo.findTop10ByOrderByKeyWordIdDesc();
+            Collections.reverse(keywordList); // 순서가 반대로 불러와져서 있어서 뒤집기
 
-			for (KeyWord k : keywordList) {
-				String keyword = k.getName();
-				List<ArticleDto> articleDtos = newsService.searchWithClusters(keyword);
-				Quiz quiz = null;
-				// 헤드라인 뉴스 선정
-				ArticleDto headLineArticle = null;
-				int count = 0;
-				for (int i = 0; i < 5 && count == 0; i++) {
-					if (articleDtos.get(i).getContent().length() <= 2000) {
-						headLineArticle = articleDtos.get(i);
-						count++;
-					}
-				}
+            for (KeyWord k : keywordList) {
+                String keyword = k.getName();
+                List<ArticleDto> articleDtos = newsService.searchWithClusters(keyword);
+                Quiz quiz = new Quiz();
 
-				if (count == 0) {
-					headLineArticle = articleDtos.get(0);
-					headLineArticle.setContent(headLineArticle.getContent().substring(0, 1998));
-				}
+                // 헤드라인 뉴스 선정
+                ArticleDto headLineArticle = null;
+                int count = 0;
+                for (int i = 0; i < 5 && count == 0; i++) {
+                    if (articleDtos.get(i).getContent().length() <= 2000) {
+                        headLineArticle = articleDtos.get(i);
+                        count++;
+                    }
+                }
 
-				String summary = getSummary(headLineArticle.getContent());
-				k.setSummary(summary);
-				k.setCategory(headLineArticle.getCategory());
-				keyWordRepo.save(k); // 키워드 업데이트
-				ArrayList<String> list = getQuiz(headLineArticle.getContent());
-				for(int i=0;i<2;i++){
-					int x=i*6;
-					quiz.setTitle(list.get(0+x));
-					quiz.setAnswer1(list.get(1+x));
-					quiz.setAnswer2(list.get(2+x));
-					quiz.setAnswer3(list.get(3+x));
-					quiz.setAnswer4(list.get(4+x));
-					quiz.setCorrectAnswer(list.get(5+x));
-					quiz.setKeyword(k);
-					quizRepo.save(quiz);
-				}
-				// 연관 뉴스 저장
-				for (ArticleDto articleDto : articleDtos) {
-					articleDto.setKeyWordId(k.getKeyWordId());
-					articleService.saveArticle(articleDto);
-				}
-			}
+                if (count == 0) {
+                    headLineArticle = articleDtos.get(0);
+                    headLineArticle.setContent(headLineArticle.getContent().substring(0, 1998));
+                }
 
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-			throw new RuntimeException(e);
-		}
-	}
+                String summary = getSummary(headLineArticle.getContent());
+                k.setSummary(summary);
+                k.setCategory(headLineArticle.getCategory());
+                keyWordRepo.save(k); // 키워드 업데이트
+                ArrayList<String> list = getQuiz(headLineArticle.getContent());
+                for (int i = 0; i < 3; i++) {
+                    int x = i * 6;
+                    quiz.setTitle(list.get(0 + x));
+                    quiz.setAnswer1(list.get(1 + x));
+                    quiz.setAnswer2(list.get(2 + x));
+                    quiz.setAnswer3(list.get(3 + x));
+                    quiz.setAnswer4(list.get(4 + x));
+                    quiz.setCorrectAnswer(list.get(5 + x));
+                    quiz.setKeyword(k);
+                    quizRepo.save(quiz);
+                }
+                // 연관 뉴스 저장
+                for (ArticleDto articleDto : articleDtos) {
+                    articleDto.setKeyWordId(k.getKeyWordId());
+                    articleService.saveArticle(articleDto);
+                }
+            }
+
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
 
 //	public static void main(String[] args) throws IOException {
 ////		int a = getTokenSize("더불어민주당 전라북도 지역 국회의원들은 정부의 새만금 SOC 예산 삭감은 전북을 잼버리 파행 희생양으로 삼은 것이라고 강하게 반발하며 규탄의 의미로 삭발식을 진행했습니다.민주당 김윤덕 의원을 비롯한 전북 의원들과 당원·지지자들은 오늘(7일) 국회 본청 앞에서 윤석열 정부 새만금 SOC 예산 삭감 규탄대회를 열었습니다.삭발에 참여한 김성주 의원은 정부가 잼버리 파행 책임을 전북에 모두 떠넘기고 있다면서, 새만금 예산의 부활을 위해 전체주의에 맞서 싸우겠다고 밝혔습니다.한병도 의원은 다음 주 기획재정부 앞에서 예산 삭감을 규탄하며 별도 삭발식을 진행할 예정입니다.※ '당신의 제보가 뉴스가 됩니다'[카카오톡] YTN 검색해 채널 추가[전화] 02-398-8585[메일] social@ytn.co.kr");
@@ -153,125 +152,132 @@ public class ArticleBatchUtil {
 //
 //	}
 
-	private String getSummary(String content) throws IOException {
-		HttpPost httpPost = new HttpPost(url);
+    private String getSummary(String content) throws IOException {
+        HttpPost httpPost = new HttpPost(url);
 
-		httpPost.setHeader("Accept", "application/json;UTF-8");
-		httpPost.setHeader("Content-Type", "application/json;UTF-8");
-		httpPost.setHeader("X-NCP-APIGW-API-KEY-ID", client_id);
-		httpPost.setHeader("X-NCP-APIGW-API-KEY", client_secret);
+        httpPost.setHeader("Accept", "application/json;UTF-8");
+        httpPost.setHeader("Content-Type", "application/json;UTF-8");
+        httpPost.setHeader("X-NCP-APIGW-API-KEY-ID", client_id);
+        httpPost.setHeader("X-NCP-APIGW-API-KEY", client_secret);
 
-		JSONObject documentObj = new JSONObject();
-		documentObj.put("content", content);
+        JSONObject documentObj = new JSONObject();
+        documentObj.put("content", content);
 
-		JSONObject optionObj = new JSONObject();
-		optionObj.put("language", "ko");
-		optionObj.put("model", "news");
-		optionObj.put("tone", 2);
-		optionObj.put("summaryCount", 2);
+        JSONObject optionObj = new JSONObject();
+        optionObj.put("language", "ko");
+        optionObj.put("model", "news");
+        optionObj.put("tone", 2);
+        optionObj.put("summaryCount", 2);
 
-		JSONObject mainObj = new JSONObject();
-		mainObj.put("document", documentObj);
-		mainObj.put("option", optionObj);
+        JSONObject mainObj = new JSONObject();
+        mainObj.put("document", documentObj);
+        mainObj.put("option", optionObj);
 
-		StringEntity entity = new StringEntity(mainObj.toString(), ContentType.APPLICATION_JSON);
-		httpPost.setEntity(entity);
+        StringEntity entity = new StringEntity(mainObj.toString(), ContentType.APPLICATION_JSON);
+        httpPost.setEntity(entity);
 
-		CloseableHttpResponse response = httpClient.execute(httpPost);
+        CloseableHttpResponse response = httpClient.execute(httpPost);
 
-		HttpEntity et = response.getEntity();
-		String resultContent = EntityUtils.toString(et, "UTF-8");
+        HttpEntity et = response.getEntity();
+        String resultContent = EntityUtils.toString(et, "UTF-8");
 
-		return resultContent.substring(11, resultContent.length() - 2);
-	}
+        return resultContent.substring(11, resultContent.length() - 2);
+    }
 
-	static private ArrayList<String> getQuiz(String context) throws IOException {
-		HttpPost httpPost = new HttpPost(url2);
+    static private ArrayList<String> getQuiz(String article) throws IOException {
 
-		httpPost.setHeader("Content-Type", "application/json");
-		httpPost.setHeader("Authorization", "Bearer "+gpt);
+        // "를 '로 변경
+        String context = article.replace("\"", "'");
 
-		JSONObject messageObj = new JSONObject();
-		messageObj.put("role","system");
-		messageObj.put("content","You are a helpful assistant.");
+        System.out.println("아래 context");
+        System.out.println(context);
+        HttpPost httpPost = new HttpPost(url2);
 
-		JSONObject messageObj1 = new JSONObject();
-		messageObj1.put("role","user");
-		messageObj1.put("content",context);
+        httpPost.setHeader("Content-Type", "application/json");
+        httpPost.setHeader("Authorization", "Bearer " + gpt);
+
+        JSONObject messageObj = new JSONObject();
+        messageObj.put("role", "system");
+        messageObj.put("content", "You are a helpful assistant.");
+
+        JSONObject messageObj1 = new JSONObject();
+        messageObj1.put("role", "user");
+        messageObj1.put("content", context);
 
 //		messageObj.put("content",context+"\n 이 뉴스 내용을 이용해서 객관식퀴즈를 내고 정답을 알려줘");
-		String t = "{\n" +
-				"    \"model\": \"gpt-3.5-turbo\",\n" +
-				"    \"messages\": [\n" +
-				"      {\n" +
-				"        \"role\": \"system\",\n" +
-				"        \"content\": \"You are a helpful assistant.\"\n" +
-				"      },\n" +
-				"      {\n" +
-				"        \"role\": \"user\",\n" +
-				"        \"content\": \"기사를 기반으로 보기가 4개인 객관식 퀴즈3개를 답과함께 만들어줘 형식을 꼭 지켜줘 띄어쓰기도 지켜 \\n 기사:"+context+"\\n\\nQuiz 1:\\n\\n질문:\\n\\n보기\\n\\nA.\\n\\nB.\\n\\nC.\\n\\nD.\\n\\n정답\\n\\nQuiz 2:\\n\\n질문: \\n\\n보기:\\n\\nA. \\n\\nB. \\n\\nC. \\n\\nD. \\n\\n정답:\\n\\nQuiz 3:\\n\\n질문: \\n\\n보기:\\n\\nA. \\n\\nB. \\n\\nC. \\n\\nD. \\n\\n정답: \""+"\n" +
-				"      }\n" +
-				"    ]\n" +
-				"  }";
+        String t = "{\n" +
+                "    \"model\": \"gpt-3.5-turbo\",\n" +
+                "    \"messages\": [\n" +
+                "      {\n" +
+                "        \"role\": \"system\",\n" +
+                "        \"content\": \"You are a helpful assistant.\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"role\": \"user\",\n" +
+                "        \"content\": \"기사를 기반으로 보기가 4개인 객관식 퀴즈3개를 답과함께 만들어줘 형식을 꼭 지켜줘 띄어쓰기도 지켜 \\n 기사:" + context + "\\n\\nQuiz 1:\\n\\n질문:\\n\\n보기\\n\\nA.\\n\\nB.\\n\\nC.\\n\\nD.\\n\\n정답\\n\\nQuiz 2:\\n\\n질문: \\n\\n보기:\\n\\nA. \\n\\nB. \\n\\nC. \\n\\nD. \\n\\n정답:\\n\\nQuiz 3:\\n\\n질문: \\n\\n보기:\\n\\nA. \\n\\nB. \\n\\nC. \\n\\nD. \\n\\n정답: \"" + "\n" +
+                "      }\n" +
+                "    ]\n" +
+                "  }";
+
+        System.out.println(t);
 //		System.out.println(t);
-		JSONObject mainObj = new JSONObject();
-		mainObj.put("model","gpt-3.5-turbo");
-		mainObj.put("message", messageObj);
-		mainObj.put("message", t);
+        JSONObject mainObj = new JSONObject();
+        mainObj.put("model", "gpt-3.5-turbo");
+        mainObj.put("message", messageObj);
+        mainObj.put("message", t);
 //		StringEntity entity = new StringEntity(mainObj.toString(), ContentType.APPLICATION_JSON);
-		StringEntity entity = new StringEntity(t, ContentType.APPLICATION_JSON);
+        StringEntity entity = new StringEntity(t, ContentType.APPLICATION_JSON);
 //		System.out.println(mainObj.toString());
-		httpPost.setEntity(entity);
+        httpPost.setEntity(entity);
 
-		CloseableHttpResponse response = httpClient.execute(httpPost);
+        CloseableHttpResponse response = httpClient.execute(httpPost);
 
-		HttpEntity et = response.getEntity();
-		InputStream is = et.getContent();
+        HttpEntity et = response.getEntity();
+        InputStream is = et.getContent();
 
 //		String resultContent = EntityUtils.toString(et, "UTF-8");
 //		System.out.println(resultContent);
-		BufferedReader r = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-		String line = null;
-		for(int i=0;i<11;i++){
-			line=r.readLine().toString();
-			System.out.println(line);
-		}
+        BufferedReader r = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+        String line = null;
+        for (int i = 0; i < 11; i++) {
+            line = r.readLine().toString();
+            System.out.println(line);
+        }
 
-		String[] qu = line.split("\\\\n");
-		for(String i : qu){
-			System.out.println(i);
-		}
-		ArrayList<String> a = new ArrayList<>();
-		for(String i : qu){
-			if(i.length()>3) {
-				if(i.charAt(1) == '문'){
-					String[] b = i.split(": ");
-					a.add(b[1]);
-				}
-				else if (i.charAt(1) == '.') {
-					String[] b = i.split(" ");
-					a.add(b[1]);
-				}
-				else if(i.charAt(1) == '답'){
-					String[] b = i.split(": ");
-					if(b[1].charAt(0)=='A'||b[1].charAt(0)=='B'||b[1].charAt(0)=='C'||b[1].charAt(0)=='D')
-						a.add(b[1].charAt(0)+"");
-					else{
-						a.add(b[1]);
-					}
-				}
-			}
-		}
+        String[] qu = line.split("\\\\n");
+        for (String i : qu) {
+            System.out.println(i);
+        }
+        ArrayList<String> a = new ArrayList<>();
+        for (String i : qu) {
+            if (i.length() > 3) {
+                if (i.charAt(1) == '문') {
+                    String[] b = i.split(": ");
+                    a.add(b[1]);
+                } else if (i.charAt(1) == '.') {
+                    String[] b = i.split(" ");
+                    a.add(b[1]);
+                } else if (i.charAt(1) == '답') {
+                    String[] b = i.split(": ");
+                    if (b[1].charAt(0) == 'A' || b[1].charAt(0) == 'B' || b[1].charAt(0) == 'C' || b[1].charAt(0) == 'D')
+                        a.add(b[1].charAt(0) + "");
+                    else {
+                        a.add(b[1]);
+                    }
+                }
+            }
+        }
 
-		return a;
-	}
-	static public int getTokenSize(String text) {
-		EncodingRegistry registry = Encodings.newDefaultEncodingRegistry();
-		Encoding enc = registry.getEncodingForModel(ModelType.GPT_3_5_TURBO);
+        return a;
+    }
 
-		List<Integer> encoded = enc.encode(text);
-		return encoded.size();
-	}
+    static public int getTokenSize(String text) {
+        EncodingRegistry registry = Encodings.newDefaultEncodingRegistry();
+        Encoding enc = registry.getEncodingForModel(ModelType.GPT_3_5_TURBO);
+
+        List<Integer> encoded = enc.encode(text);
+        return encoded.size();
+    }
 
 }
 
