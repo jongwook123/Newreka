f88a7ff5129f2aa4e86f6c6fb1c9606e16d56d0d
윤우혁 <squareyun@gmail.com>
2023-10-05 14:43:42 +0900

    Fix: Quiz 엔티티 수정

--- backend/src/main/java/com/d103/newreka/quiz/domain/Quiz.java
+++ backend/src/main/java/com/d103/newreka/quiz/domain/Quiz.java
@@ -1,26 +1,15 @@
 package com.d103.newreka.quiz.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-
 import com.d103.newreka.hottopic.domain.KeyWord;
 import com.d103.newreka.user.domain.QuizState;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -28,32 +17,32 @@ import lombok.NoArgsConstructor;
 @Getter
 @Builder
 public class Quiz {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "quiz_id", nullable = false)
-	private Long quizId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "quiz_id", nullable = false)
+    private Long quizId;
 
-	@Column(nullable = false, columnDefinition = "varchar(100)")
-	private String title;
+    @Column(nullable = false, columnDefinition = "varchar(100)")
+    private String title;
 
-	@Column(nullable = false, columnDefinition = "varchar(20)")
-	private String answer1;
-	@Column(nullable = false, columnDefinition = "varchar(20)")
-	private String answer2;
-	@Column(nullable = false, columnDefinition = "varchar(20)")
-	private String answer3;
-	@Column(nullable = false, columnDefinition = "varchar(20)")
-	private String answer4;
+    @Column(nullable = false, columnDefinition = "varchar(200)")
+    private String answer1;
+    @Column(nullable = false, columnDefinition = "varchar(200)")
+    private String answer2;
+    @Column(nullable = false, columnDefinition = "varchar(200)")
+    private String answer3;
+    @Column(nullable = false, columnDefinition = "varchar(200)")
+    private String answer4;
 
-	@Column(nullable = false, columnDefinition = "int")
-	private int collectAnswer;
+    @Column(nullable = false, columnDefinition = "int")
+    private Integer collectAnswer;
 
-	@ManyToOne
-	@JoinColumn(name = "keyWord_id", nullable = false)
-	private KeyWord keyword;
+    @ManyToOne
+    @JoinColumn(name = "keyWord_id", nullable = false)
+    private KeyWord keyword;
 
-	@Builder.Default
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "quiz", cascade = CascadeType.ALL)
-	private List<QuizState> quizStates = new ArrayList<>();
+    @Builder.Default
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "quiz", cascade = CascadeType.ALL)
+    private List<QuizState> quizStates = new ArrayList<>();
 
 }
--- backend/src/main/java/com/d103/newreka/quiz/dto/QuizDto.java
+++ backend/src/main/java/com/d103/newreka/quiz/dto/QuizDto.java
@@ -1,7 +1,6 @@
 package com.d103.newreka.quiz.dto;
 
 import com.d103.newreka.quiz.domain.Quiz;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,29 +11,29 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class QuizDto {
-	private Long quizId;
-	private String title;
-	private String answer1;
-	private String answer2;
-	private String answer3;
-	private String answer4;
-	private int collectAnswer;
-	private Long keyword;
+    private Long quizId;
+    private String title;
+    private String answer1;
+    private String answer2;
+    private String answer3;
+    private String answer4;
+    private Integer collectAnswer;
+    private Long keyword;
 
-	public static QuizDto fromEntity(Quiz quiz) {
-		//        TeamRepo teamRepo = null;
-		//        StadiumRepo stadiumRepo = null;
-		return QuizDto.builder()
-			.quizId(quiz.getQuizId())
-			.title(quiz.getTitle())
-			.answer1(quiz.getAnswer1())
-			.answer2(quiz.getAnswer2())
-			.answer3(quiz.getAnswer3())
-			.answer4(quiz.getAnswer4())
-			.collectAnswer(quiz.getCollectAnswer())
-			.keyword(quiz.getKeyword().getKeyWordId())
-			.build();
-	}
+    public static QuizDto fromEntity(Quiz quiz) {
+        //        TeamRepo teamRepo = null;
+        //        StadiumRepo stadiumRepo = null;
+        return QuizDto.builder()
+                .quizId(quiz.getQuizId())
+                .title(quiz.getTitle())
+                .answer1(quiz.getAnswer1())
+                .answer2(quiz.getAnswer2())
+                .answer3(quiz.getAnswer3())
+                .answer4(quiz.getAnswer4())
+                .collectAnswer(quiz.getCollectAnswer())
+                .keyword(quiz.getKeyword().getKeyWordId())
+                .build();
+    }
 
 }
 
