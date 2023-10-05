7b2660499837418df5fccaa040164f5f144e7c4f
윤우혁 <squareyun@gmail.com>
2023-10-05 23:52:13 +0900

    Chore: test

--- backend/src/main/java/com/d103/newreka/user/service/ScrapService.java
+++ backend/src/main/java/com/d103/newreka/user/service/ScrapService.java
@@ -52,11 +52,10 @@ public class ScrapService {
     public List<Scrap> getScrapCategoryList(User user, String category) {
         List<Scrap> scr = scrapRepo.findByUserId_Id(user.getId());
         List<Scrap> scraps = new ArrayList<>();
-        System.out.println("category: " + category + user.getId());
+        System.out.println("category: " + category + " userid: " user.getId());
         for (int i = 0; i < scr.size(); i++) {
             Scrap curScr = scr.get(i);
 
-            System.out.println(curScr);
             System.out.println(curScr.getArticleId().getCategory());
             if (scr.get(i).getArticleId().getCategory().equals(category))
                 scraps.add(scr.get(i));
