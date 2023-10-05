d5ea9b64a59950e995721f44d91a99f2c6dfdcfd
jeonghwanKim <doyun30@naver.com>
2023-10-05 20:01:30 +0900

    Feat. QuizState 키워드 불러오기

--- backend/src/main/java/com/d103/newreka/user/repo/QuizStateRepo.java
+++ backend/src/main/java/com/d103/newreka/user/repo/QuizStateRepo.java
@@ -5,6 +5,10 @@ import com.d103.newreka.user.domain.QuizState;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuizStateRepo extends JpaRepository<QuizState, Long> {
+
+    List<QuizState> findByUser_IdAndCategory(Long id, String category);
 }
--- backend/src/main/java/com/d103/newreka/user/service/QuizStateService.java
+++ backend/src/main/java/com/d103/newreka/user/service/QuizStateService.java
@@ -3,12 +3,16 @@ package com.d103.newreka.user.service;
 import com.d103.newreka.hottopic.repo.KeyWordRepo;
 import com.d103.newreka.quiz.repo.QuizRepo;
 import com.d103.newreka.user.domain.QuizState;
+import com.d103.newreka.user.domain.User;
 import com.d103.newreka.user.dto.QuizStateDto;
 import com.d103.newreka.user.repo.QuizStateRepo;
 import com.d103.newreka.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -30,4 +34,31 @@ public class QuizStateService {
         quizStateRepo.save(quizState);
     }
 
+    @Transactional
+    public HashMap<String,Integer> myKeyWord(User user){
+
+        HashMap<String,Integer> keyWordList = new HashMap<>();
+
+        System.out.println("잘 들어는 옴");
+
+        List<QuizState> list = quizStateRepo.findByUser_IdAndCategory(user.getId(),"사회");
+        keyWordList.put("사회",list.size());
+        list = quizStateRepo.findByUser_IdAndCategory(user.getId(),"경제");
+        keyWordList.put("경제",list.size());
+        list = quizStateRepo.findByUser_IdAndCategory(user.getId(),"정치");
+        keyWordList.put("정치",list.size());
+        list = quizStateRepo.findByUser_IdAndCategory(user.getId(),"세계");
+        keyWordList.put("세계",list.size());
+        list = quizStateRepo.findByUser_IdAndCategory(user.getId(),"IT");
+        keyWordList.put("IT",list.size());
+        list = quizStateRepo.findByUser_IdAndCategory(user.getId(),"생활");
+        keyWordList.put("생활",list.size());
+        list = quizStateRepo.findByUser_IdAndCategory(user.getId(),"오피니언");
+        keyWordList.put("오피니언",list.size());
+
+        System.out.println("실행 잘 됨");
+
+        return keyWordList;
+    }
+    //사회, 경제, 정치, 세계, IT, 생활, 오피니언
 }
