eb7b64fab4e9daa26bb5d14239760e7cee79122a
윤우혁 <squareyun@gmail.com>
2023-10-06 15:32:48 +0900

    Docs: 불필요한 파일 삭제

--- crawler/chapter02_beautifulsoup.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-response = requests.get("https://www.naver.com/")
-html = response.text
-soup = BeautifulSoup(html, 'html.parser')
-word = soup.select_one('title')
-print(word.text)
